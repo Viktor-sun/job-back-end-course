6fe989d36f20f71081faf514a58923420f09906c
Viktor-sun <bobick.zdoh@gmail.com>
2023-10-04 19:20:54 +0300

    add docker file and helmet

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
+import helmet from "helmet";
+
 const app = express();
 app.use(express.json());
 app.use(
@@ -8,6 +10,7 @@ app.use(
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
   })
 );
+app.use(helmet());
 
 const PORT = 8000;
 
